8fc647cca7d194dffb3d414d34b54e49b6fa94bf
hcc007 <hcwang1024@163.com>
2020-08-25 22:00:51 +0800

    首页完成，接口不能用

---
+++ app/src/main/java/com/hc/kotlinvideo/adapter/YueDanAdapter.kt
@@ -0,0 +1,9 @@
+package com.hc.kotlinvideo.adapter
+
+/**
+ * Created by hcw  on 2020/8/25
+ * 类描述：
+ * all rights reserved
+ */
+class YueDanAdapter {
+}
---
+++ app/src/main/java/com/hc/kotlinvideo/widget/YueDanItemView.kt
@@ -0,0 +1,9 @@
+package com.hc.kotlinvideo.widget
+
+/**
+ * Created by hcw  on 2020/8/25
+ * 类描述：
+ * all rights reserved
+ */
+class YueDanItemView {
+}
